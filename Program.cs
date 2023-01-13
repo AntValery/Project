f352ffa7494593bdcf59a1bec45ca1a453983db5
1valerich1@mail.ru <1valerich1@mail.ru>
2023-01-13 08:25:50 +0400

    Добавлена программа

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string[] CreateArray(string[] array){
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++){
+        if (array[i].Length <= 3) count++;
+    }
+
+    string[] newArray = new String[count];
+    count = 0;
+
+    for(int i = 0; i < array.Length; i++){
+        if(array[i].Length <= 3){
+            newArray[count] = array[i];
+            count++;
+        }  
+    }
+
+    return newArray;
+}
+
+void PrintArray(string[] array){
+    for(int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+}
+
+string[] array = {"hello", "2", "world", ":-)"};
+PrintArray(CreateArray(array));
